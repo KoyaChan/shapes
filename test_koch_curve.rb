b5246f0db7b1abf0440e7688eaad1d0d97830f65
KoyaChan <koyto01@yahoo.co.jp>
2018-04-16 17:55:26 +0900

    add test_triangle in test_koch_curve.rb

--- test_koch_curve.rb
+++ test_koch_curve.rb
@@ -3,11 +3,24 @@ require './koch_curve'
 
 class TestKochCurve < Test::Unit::TestCase
   def setup
-    @location = Segment::Location.new(100.0 / 3.0, 0)
+    @location = Segment::Location.new(x: 100.0 / 3.0, y: 0)
     @segment = Segment.new(p1: @location,
                            radian: Math::PI * 1.0 / 3.0,
                            length: 100.0 / 3.0)
   end
+
+  def test_triangle
+    p1 = Location.new(x: 0, y: 0)
+    p2 = Location.new(x: 10, y: 0)
+    segment = Segment.new(p1: p1, p2: p2)
+    koch = KochCurve.new
+    triangle = koch.triangle(segment)
+    assert_equal 2, triangle.size
+    assert_equal Segment, triangle[0].class
+    assert_equal Segment::Pizza_radian, triangle[0].radian
+    assert_equal (Segment::Pizza_radian * 5).round(3), triangle[1].radian
+  end
+
 
   def test_next_step_returns_array_with_4_segments
     koch = KochCurve.new(base: @segment)
@@ -19,3 +32,6 @@ class TestKochCurve < Test::Unit::TestCase
     end
   end
 end
+
+# Todo
+# - Segmentを負の方向へrotateしたあと、segment.radianの値を再計算させる
