f0750cf8aaf634a00a39ebf24c39484dc095c1fc
KoyaChan <koyto01@yahoo.co.jp>
2018-05-02 16:54:01 +0900

    make closed koch curve

--- koch_curve.rb
+++ koch_curve.rb
@@ -55,21 +55,41 @@ class KochCurve
   end
 end
 
-width = 2000
+width = 1600
 height = 1600
 format = Cairo::FORMAT_ARGB32
 stride = Cairo::Format.stride_for_width(format, width)
 data = '\0' * stride * height
 surface = Cairo::ImageSurface.new(data, format, width, height, stride)
 context = Cairo::Context.new(surface)
-context.set_source_color(Cairo::Color::SLATE_GRAY)
+context.set_source_color(Cairo::Color::ALICE_BLUE)
 context.rectangle(0, 0, width, height)
 context.fill
 
-context.set_source_color(Cairo::Color::HOT_MAGENTA)
-origin = Location.new(x: 0, y: 100)
-base = Segment.new(p1: origin, length: width)
-koch = KochCurve.new(base: base)
 count = 6
+context.set_source_color(Cairo::Color::HOT_MAGENTA)
+origin = Location.new(x: 50, y: 100)
+base = Segment.new(p1: origin, length: width - 100)
+koch = KochCurve.new(base: base)
 koch.draw(context, count)
+
+origin = base.p2
+p2 = Location.new(x: origin.x, y: height - 100)
+base1 = Segment.new(p1: origin, p2: p2)
+koch1 = KochCurve.new(base: base1)
+koch1.draw(context, count)
+
+origin = base1.p2
+p2 = Location.new(x: 50, y: origin.y)
+base2 = Segment.new(p1: origin, p2: p2)
+koch2 = KochCurve.new(base: base2)
+koch2.draw(context, count)
+
+origin = base2.p2
+p2 = base.p1
+base3 = Segment.new(p1: origin, p2: p2)
+koch3 = KochCurve.new(base: base3)
+koch3.draw(context, count)
+
+
 surface.write_to_png("./curve#{count}.png")
